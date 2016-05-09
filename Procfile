d1295684cc25dfcbf5a461c8acaf3274caf122e4
Viviana Terceros <vivitc@gmail.com>
2016-05-08 23:27:37 -0300

    Changes structure to be ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn publication_manager.wsgi -w 1
---
+++ local.py
@@ -0,0 +1,11 @@
+DATABASES = {
+   'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+    }
+}
